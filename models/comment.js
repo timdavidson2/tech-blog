5db04d45ada31d8cd5d15a54183618dde21291e1
Tim Davidson <timdavidson2@gmail.com>
2021-09-14 08:02:52 -0400

    Update comment.js

--- models/comment.js
+++ models/comment.js
@@ -1,3 +1,12 @@
 const { Model, DataTypes } = require("sequelize");
 const sequelize = require("../config/connection");
 class Comment extends Model {}
+
+Comment.init({
+  id: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    primaryKey: true,
+    autoIncrement: true,
+  },
+});
