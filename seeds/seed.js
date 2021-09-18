e5f1a2dae0acbe43043d03f60324d47ee696575b
Tim Davidson <timdavidson2@gmail.com>
2021-09-18 11:11:51 -0400

    Update seed.js

--- seeds/seed.js
+++ seeds/seed.js
@@ -1,3 +1,7 @@
 const sequelize = require('../config/conection');
 const { User, Blog, Comment } = require('../models');
 
+const blogData = require('./blogdata.json');
+const commentData = require('./commentData.json');
+const userData = require('./userData.json');
+
